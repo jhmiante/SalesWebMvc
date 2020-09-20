edc5980cbffe6d9f5c8ed56e70aee15db93e73f5
jhmiante <jhmiante@gmail.com>
2020-09-20 18:06:31 -0300

    Primeiro controller e testes nas páginas

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App - Curso C#";
+            ViewData["professor"] = "Nélio Alves";
+            ViewData["aluno"] = "Jonathas H. Miante";
 
             return View();
         }
